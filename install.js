53cc44cd4f3c0bbb6cab7c3c0492ad5b62ddfd1d
Peter Ortiz <peterortiz82@gmail.com>
2017-03-25 13:15:12 -0400

    updated install.js to store creds to dynamodb

--- install.js
+++ install.js
@@ -2,7 +2,10 @@
 
 /* eslint-disable no-console */
 const qs = require('querystring');
+const AWS = require('aws-sdk');
 const fetch = require('node-fetch');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const getCode = (event) => {
   let code = null;
@@ -30,6 +33,15 @@ const requestToken = (code) => {
     });
 };
 
+const saveResponse = (response) => {
+  const params = {
+    TableName: process.env.TEAMS_TABLE,
+    Item: response,
+  };
+  console.log('Put', params);
+  return dynamodb.put(params).promise();
+};
+
 const successResponse = callback => callback(null, {
   statusCode: 302,
   headers: { Location: process.env.INSTALL_SUCCESS_URL },
@@ -47,6 +59,6 @@ module.exports.handler = (event, context, callback) =>
   Promise.resolve(event)
     .then(getCode) // Get code from event
     .then(requestToken) // Exchange code for token
-    // .then(saveResponse) // Save token to DDB
+    .then(saveResponse) // Save token to DDB
     .then(() => successResponse(callback))
     .catch(error => errorResponse(error, callback));
